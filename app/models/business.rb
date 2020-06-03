6ab6237ed7d02038529ce8fed25c7263a77f6eff
Mely07 <damely.tineo07@gmail.com>
2020-06-03 12:06:02 -0400

    Set up home route.

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,6 @@
 class Business < ApplicationRecord
     has_many :philanthropic_initiatives
     has_many :beneficiaries, through: :philanthropic_initiatives
+
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  
+  
   resources :philanthropic_initiatives
   resources :beneficiaries
   resources :businesses
