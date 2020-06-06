df319855382a10fd63095596115b0992723ef91e
Mely07 <damely.tineo07@gmail.com>
2020-06-06 12:04:36 -0400

    Create method creates a new initative and associates it to a business and beneficiary via id.

--- app/controllers/philanthropic_initiatives_controller.rb
+++ app/controllers/philanthropic_initiatives_controller.rb
@@ -1,2 +1,8 @@
 class PhilanthropicInitiativesController < ApplicationController
+    def create 
+        @business = Business.find_by(id: session[:business_id])
+        @beneficiary = Beneficiary.find_by(id: params[:beneficiary_id])
+        
+        @philanthropic_initiative = PhilanthropicInitiative.new(business: @business, beneficiary: @beneficiary)
+    end
 end
